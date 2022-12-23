80948acb78cd9bdbd9472fe7dc41b3de9ec57384
kilik42 <marvinevins@yahoo.com>
2022-12-23 16:39:34 -0600

    imported files into app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {CssBaseline, Grid} from '@material-ui/core';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 function App() {
   return (
