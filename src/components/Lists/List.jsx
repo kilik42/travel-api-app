26de08df8140eef3178b31fdd2a4a6175e749720
kilik42 <marvinevins@yahoo.com>
2022-12-23 19:49:05 -0600

    working on listitem component

--- src/components/Lists/List.jsx
+++ src/components/Lists/List.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {Paper, Typography, Button, useMediaQuery} from '@material-ui/core';
+import {makeStyles, useTheme} from '@material-ui/core/styles';
+import {Rating} from '@material-ui/lab';
+import {CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select} from '@material-ui/core';
 
 const List = () => {
+  const classes = useStyles();
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
+
   return (
-    <div>List</div>
+    <div className={classes.container}>
+      <Typography variant="h4">Restaurants, Hotels and Attractions around you</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e)=> setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={type} onChange={(e)=> setType(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+          
+        </Select>
+      </FormControl>
+
+    </div>
   )
 }
 
